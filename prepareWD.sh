8157560a4f967139a3f5140fde9e233c1773332a
Leon Ramos <lramos@cubiedev.(none)>
2016-09-25 16:11:19 -0500

    Fixed if directory

--- prepareWD.sh
+++ prepareWD.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -n "Detecting linux-sunxi kernel..."
-if [ -z linux-sunxi ] ; then
+if [ ! -d "linux-sunxi" ] ; then
 	echo "Not found, downloading!"
 	git clone https://github.com/linux-sunxi/linux-sunxi.git
 else 
@@ -9,7 +9,7 @@ else
 fi
 
 echo -n "Detecting sunxi-bsp tools..."
-if [ -z sunxi-bsp ] ; then
+if [ ! -d "sunxi-bsp" ] ; then
 	echo "Not found, downloading!"
 	git clone https://github.com/linux-sunxi/sunxi-bsp.git
 else
